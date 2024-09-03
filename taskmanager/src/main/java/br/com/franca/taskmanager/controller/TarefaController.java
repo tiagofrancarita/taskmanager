467cb55168144d3f61ba4d11deaf25ad50826632
Tiago França <tiagofranca.rita@gmail.com>
2024-09-03 13:36:58 -0300

    alter tarefacontroller

--- taskmanager/src/main/java/br/com/franca/taskmanager/controller/TarefaController.java
+++ taskmanager/src/main/java/br/com/franca/taskmanager/controller/TarefaController.java
@@ -8,12 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/tarefas")
+@RequestMapping("v1/tarefas")
 public class TarefaController {
 
         @Autowired
